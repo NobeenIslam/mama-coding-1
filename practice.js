f7ec7ce2bbc89217e7005b7d2e0ffbba61bd9c91
MJOO9 <106614930+MJOO9@users.noreply.github.com>
2022-07-25 20:10:25 +0600

    myfirstCommit

--- practice.js
+++ practice.js
@@ -1,8 +1,22 @@
 "use strict"
-let array = ["bbo","tim","vaf","faf","afs"]
+    function findTheBiggestNum(listNum){
 
-for (let i=0;i<=10; i++){
-    console.log(i)
+            let biggestnum=listNum[0]
+
+            for(const myNumber of listNum){
+                console.log(myNumber)
+                if(myNumber<biggestnum){
+                    biggestnum=myNumber
+                }
+            }
+            return biggestnum
+
+// const array= [1,2,3,4,5,]
+// console.log(array[1])
+
 }
+console.log(findTheBiggestNum([10,100,2001,20]))
+console.log("hello world")
 
-console.log(array[2])
+
+
