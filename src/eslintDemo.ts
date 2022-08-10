b660a9e9a84ceaa843d2ed67416454d30f1446e0
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-08-10 15:24:42 +0100

    Added CI and moved into Src

--- src/eslintDemo.ts
+++ src/eslintDemo.ts
@@ -5,11 +5,11 @@ function addCoins(coins: number): number {
   return coins + 2;
 }
 
-function nameChecker(name:string){
-    let statement = "mama is not here"
-    if(name === "Mama"){
-        statement = "mama is here"
-    }
-    return statement 
+function nameChecker(name: string) {
+  let statement = "mama is not here";
+  if (name === "Mama") {
+    statement = "mama is here";
+  }
+  return statement;
 }
 console.log(nameChecker("nobeen"));
