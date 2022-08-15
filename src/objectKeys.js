19290016d345cb597aaf1d4c51e17be601393a40
Nobeen Islam <40375177+NobeenIslam@users.noreply.github.com>
2022-08-15 18:20:27 +0100

    formatting and linting

--- src/objectKeys.js
+++ src/objectKeys.js
@@ -1,27 +1,27 @@
-"use strict"
-const tony = {name:"tony", age: 7, home:"Birmingam"}
-const beth = {name:"beth", age: 7, home:"London"}
-const Jissan = {name:"jissan", age: 29, home:"Bangladesh"}
+"use strict";
+const tony = { name: "tony", age: 7, home: "Birmingam" };
+const beth = { name: "beth", age: 7, home: "London" };
+const Jissan = { name: "jissan", age: 29, home: "Bangladesh" };
 
-const people = {tony,beth,Jissan}
-const peopleArr = [tony,beth,Jissan]
+const people = { tony, beth, Jissan };
+const peopleArr = [tony, beth, Jissan];
 
-tony.age = 14
-tony["favouriteFood"] = "sandwich"
-tony.favourteDrink = "milk"
+tony.age = 14;
+tony["favouriteFood"] = "sandwich";
+tony.favourteDrink = "milk";
 //console.log(tony)
 
-const keyArray = ["favouriteFood","favouriteDrink","favouriteShow"]
+const keyArray = ["favouriteFood", "favouriteDrink", "favouriteShow"];
 
-for(const person of peopleArr){
-    //console.log(`This person is ${person.name}`)
-    for(const key of keyArray){
-        //console.log(`The value of key is ${key}`)
-        person[key] = 3
-        //person.key = 3
-        //tony["favouriteFood"] = 3
-        //tony.favouriteFood
-    }  
+for (const person of peopleArr) {
+  //console.log(`This person is ${person.name}`)
+  for (const key of keyArray) {
+    //console.log(`The value of key is ${key}`)
+    person[key] = 3;
+    //person.key = 3
+    //tony["favouriteFood"] = 3
+    //tony.favouriteFood
+  }
 }
 
-console.log(peopleArr)
+console.log(peopleArr);
